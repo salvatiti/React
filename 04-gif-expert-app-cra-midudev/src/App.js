52472966526ac65dda1b7c3c0b25dfd7356aed6a
Salva <salvatiti1@gmail.com>
2022-10-25 12:28:16 +0200

    UseState,UseEffect

--- 04-gif-expert-app-cra-midudev/src/App.js
+++ 04-gif-expert-app-cra-midudev/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const GIFS = [
+  'https://media2.giphy.com/media/yJ1KSiTxaAw5G/200.webp?cid=ecf05e47cxpmzjtwkchcfyopovdweli47yf4jx47p6kizsj5&rid=200.webp&ct=g',
+  'https://media0.giphy.com/media/SWjCswum5dc0E/100.webp?cid=ecf05e47cxpmzjtwkchcfyopovdweli47yf4jx47p6kizsj5&rid=100.webp&ct=g'
+];
+
+
 function App() {
+  const  [gifs, setGifs] = useState([GIFS]);
+  const aaaaaa = 1;
+  useEffect(function () {
+    console.log('hola')
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif} / >)
+        }
+       
+      </section>
     </div>
   );
-}
+} 
 
 export default App;
